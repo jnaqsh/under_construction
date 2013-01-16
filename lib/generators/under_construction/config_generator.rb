ecd0440ba25aadc91a8f5b2116c11a0fce3b7798
Arash Mousavi <mousavi.arash@gmail.com>
2013-01-16 21:56:52 +0330

    fix issues with config generator and its test

--- lib/generators/under_construction/config_generator.rb
+++ lib/generators/under_construction/config_generator.rb
@@ -5,15 +5,11 @@ module UnderConstruction
     class ConfigGenerator < ::Rails::Generators::Base
       desc 'Configs application controller'
       def edit_application_Controller
-        # if File.exist?("app/controllers/application_controller.rb")
+        if File.exist?(Rails.root + "app/controllers/application_controller.rb")
           insert_into_file "app/controllers/application_controller.rb", CONFIG_TXT, :after => "protect_from_forgery"
-        # else
-          # raise 'Couldn\'t find application_controller file'
-        # end
-      end
-
-      def schedule_the_end_time
-        UnderConstruction::Schedule.new('3 seconds from now')
+        else
+          raise 'Couldn\'t find application_controller file'
+        end
       end
     end
   end
--- spec/generators/config_generator_spec.rb
+++ spec/generators/config_generator_spec.rb
@@ -20,6 +20,20 @@ describe UnderConstruction::Generators::ConfigGenerator do
   it "configs the application controller" do
     run_generator
     f = file('app/controllers/application_controller.rb')
-    f.should contain(/before_filter :redirect_to_under_construction/)
+    f.should contain(/#{UnderConstruction::Generators::CONFIG_TXT}/)
+  end
+
+  describe 'when application_controller.rb does not exist' do
+    let(:real_application_controller_path ) { Rails.root + 'app/controllers/application_controller.rb'}
+    let(:stash_application_controller_path) { 'stash_application_controller.rb'}
+    before do
+      FileUtils.mv real_application_controller_path, stash_application_controller_path
+    end
+    after do
+      FileUtils.mv stash_application_controller_path, real_application_controller_path
+    end
+    it 'should raise an error' do
+      expect { run_generator }.to raise_error "Couldn't find application_controller file"
+    end
   end
 end
