c1401c7b71a3b25f042ce0a6b6aa5b4bc775f543
Arash Mousavi <mousavi.arash@gmail.com>
2013-01-20 16:53:34 +0330

    clean under_construction.rb

--- lib/under_construction.rb
+++ lib/under_construction.rb
@@ -1,17 +1,12 @@
+require 'rails'
 require 'rufus-scheduler'
 require 'chronic'
 
-module UnderConstruction
-  autoload :Schedule, "under_construction/schedule"
-end
-
-# load Rails/Railtie
-begin
-  require 'rails'
-rescue LoadError
-  #do nothing
-end
-
-require "under_construction/config"
-require "under_construction/load_config"
 require "under_construction/engine"
+require "under_construction/config"
+
+module UnderConstruction
+  autoload :Schedule,       "under_construction/schedule"
+  autoload :LoadConfig,     "under_construction/load_config"
+  autoload :Engine,         "under_construction/engine"
+end
