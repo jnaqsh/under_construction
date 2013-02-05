dcaee4f92460c24959f43f09ce6ceedc2bb90557
Arash Mousavi <mousavi.arash@gmail.com>
2013-02-05 15:56:13 +0330

    use initialization hook in scheduler to fix a weird issue

--- lib/generators/under_construction/templates/under_construction_scheduler.rb
+++ lib/generators/under_construction/templates/under_construction_scheduler.rb
@@ -1,4 +1,6 @@
 # This line will initialize a scheduler to remove 
 # under_construction configs after running your server
 
-UnderConstruction::Schedule.new
+Rails.application.config.after_initialize do
+  UnderConstruction::Schedule.new
+end
