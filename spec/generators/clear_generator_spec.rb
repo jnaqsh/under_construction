abaa87a20f31ae58fd32268683de5bfff0ca9b91
Arash Mousavi <mousavi.arash@gmail.com>
2014-06-11 22:58:24 +0430

    fix tests

--- spec/generators/clear_generator_spec.rb
+++ spec/generators/clear_generator_spec.rb
@@ -1,56 +1,60 @@
 require 'spec_helper'
 require 'generators/under_construction/clear_generator'
 
-def copy_files
-  application_controller = File.expand_path("../templates/clear/application_controller.rb", __FILE__)
-  destination = File.join(destination_root, 'app', 'controllers')
-
-  FileUtils.mkdir_p(destination)
-  FileUtils.cp application_controller, destination
-
-  routes_config = File.expand_path("../templates/clear/routes.rb", __FILE__)
-  destination = File.join(destination_root, "config")
-
-  FileUtils.mkdir_p(destination)
-  FileUtils.cp routes_config, destination
-end
-
-describe UnderConstruction::Generators::ClearGenerator do
+describe UnderConstruction::Generators::ClearGenerator, :type => :generator do
   destination File.expand_path("../../../tmp", __FILE__)
 
   before do
     prepare_destination
-    copy_files
-  end
-
-  it "clears configs from application_controller" do
-    run_generator
-    f = file('app/controllers/application_controller.rb')
-    f.should_not contain('before_filter :redirect_to_under_construction')
-  end
-
-  it "clear route file" do
-    run_generator
-    f = file('config/routes.rb')
-    f.should_not contain(/match "\/\*other"/)
-    f.should_not contain(/resources 'under_construction'/)
-    f.should contain('match "under_construction", :to => redirect(\'/\')')
-  end
-
-#  it "removes view files" do
-#    f = file('app/views/under_construction')
-#    FileUtils.mkdir_p f
-#    run_generator
-#    file(f).should_not exist
-#  end
-
-  it "remove scheduler file" do
-    f = file('config/initializers')
-    FileUtils.mkdir_p f
-    FileUtils.touch f + '/under_construction_scheduler.rb'
-    # Something is wrong with 'ammeter' :( I have to stub File to make this work :/
+    copy_file(
+      File.expand_path("../templates/clear/application_controller.rb", __FILE__),
+      File.join(destination_root, 'app', 'controllers')
+    )
+    copy_file(
+      File.expand_path("../templates/clear/routes.rb", __FILE__),
+      File.join(destination_root, "config")
+    )
     File.stub(:exist?) { true }
-    run_generator
-    file(f + '/under_construction_scheduler.rb').should_not exist
+  end
+
+  it 'should run all tasks in clear generator' do
+    gen = generator %w(clear)
+    gen.should_receive :clear_application_controller
+    gen.should_receive :clear_routes_file
+    gen.should_receive :remove_scheduler_file
+    capture(:stdout) { gen.invoke_all }
+  end
+
+  context 'sth' do
+    before do
+      run_generator
+    end
+
+    it "clears configs from application_controller" do
+      f = file('app/controllers/application_controller.rb')
+      f.should_not contain('before_filter :redirect_to_under_construction')
+    end
+
+    it "clear route file" do
+      f = file('config/routes.rb')
+      f.should_not contain(/match "\/\*other"/)
+      f.should_not contain(/resources 'under_construction'/)
+      f.should contain('match "under_construction", :to => redirect(\'/\'), via: :all')
+    end
+  end
+
+  context 'remove scheduler' do
+    let(:path) { destination_root + '/config/initializers' }
+
+    before do
+      FileUtils.mkdir_p path
+      FileUtils.touch path + '/under_construction_scheduler.rb'
+      run_generator
+    end
+
+    it "remove scheduler file" do
+      File.unstub(:exist?)
+      file(path + '/under_construction_scheduler.rb').should_not exist
+    end
   end
 end
