c73b5f50f1cfd5f3276a43357712aa82d2575997
Arash Mousavi <mousavi.arash@gmail.com>
2014-06-13 19:39:14 +0430

    edit application controller config generator to correctly insert data

--- lib/generators/under_construction/config_generator.rb
+++ lib/generators/under_construction/config_generator.rb
@@ -8,7 +8,7 @@ module UnderConstruction
       desc 'Configs application controller'
       def edit_application_Controller
         if File.exist?(Rails.root + "app/controllers/application_controller.rb")
-          insert_into_file "app/controllers/application_controller.rb", APPLICATION_CONFIG_TXT, :after => "protect_from_forgery"
+          insert_into_file "app/controllers/application_controller.rb", APPLICATION_CONFIG_TXT, :after => /protect_from_forgery.*/
         else
           raise 'Couldn\'t find application_controller file'
         end
